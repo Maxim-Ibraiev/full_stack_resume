8b16680908fd3f2ba862fa81e4c14708b01e99ff
Maxim-Ibraiev <64267252+Maxim-Ibraiev@users.noreply.github.com>
2025-07-25 10:57:21 +0200

    add veet comfig

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,16 @@
-import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
+import { resolve } from "path";
+import { defineConfig } from "vite";
+
 export default defineConfig({
   plugins: [tailwindcss()],
+  base: "/full_stack_resume/",
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        nested: resolve(__dirname, "src/ua.html"),
+      },
+    },
+  },
 });
